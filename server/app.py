b875ddf0b5ceaf459f5046c606fc2c17840ff90f
michael mirhom <micharby73@gmail.com>
2023-09-23 21:12:17 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -1,26 +1,152 @@
-#!/usr/bin/env python3
-
-from flask import request, session
-from flask_restful import Resource
+from flask import Flask, request, jsonify, session
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import MetaData
+from models import User
 
-from config import app, db, api
-from models import User, Recipe
+
+
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
+
+
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        bio = data.get('bio')
+        image_url = data.get('image_url')
+
+        try:
+            user = User(username=username, password=password, bio=bio, image_url=image_url)
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return jsonify(
+                user_id=user.id,
+                username=user.username,
+                image_url=user.image_url,
+                bio=user.bio
+            ), 201
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify(message='Username already exists'), 422
+
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return jsonify(
+                    user_id=user.id,
+                    username=user.username,
+                    image_url=user.image_url,
+                    bio=user.bio
+                ), 200
+        return jsonify(error='Unauthorized'), 401
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return jsonify(
+                user_id=user.id,
+                username=user.username,
+                image_url=user.image_url,
+                bio=user.bio
+            ), 200
+        return jsonify(error='Unauthorized'), 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if 'user_id' in session:
+            session.pop('user_id', None)
+            return '', 204
+        return jsonify(error='Unauthorized'), 401
+
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            recipes = Recipe.query.filter_by(user_id=user_id).all()
+            recipe_list = []
+            for recipe in recipes:
+                recipe_list.append({
+                    'title': recipe.title,
+                    'instructions': recipe.instructions,
+                    'minutes_to_complete': recipe.minutes_to_complete,
+                    'user': {
+                        'user_id': user_id,
+                        'username': User.query.get(user_id).username,
+                        'image_url': User.query.get(user_id).image_url,
+                        'bio': User.query.get(user_id).bio
+                    }
+                })
+            return jsonify(recipes=recipe_list), 200
+        return jsonify(error='Unauthorized'), 401
+
+    def post(self):
+        user_id = session.get('user_id')
+        if user_id:
+            data = request.get_json()
+            title = data.get('title')
+            instructions = data.get('instructions')
+            minutes_to_complete = data.get('minutes_to_complete')
+            try:
+                recipe = Recipe(
+                    user_id=user_id,
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete
+                )
+                db.session.add(recipe)
+                db.session.commit()
+                return jsonify(
+                    title=recipe.title,
+                    instructions=recipe.instructions,
+                    minutes_to_complete=recipe.minutes_to_complete,
+                    user={
+                        'user_id': user_id,
+                        'username': User.query.get(user_id).username,
+                        'image_url': User.query.get(user_id).image_url,
+                        'bio': User.query.get(user_id).bio
+                    }
+                ), 201
+            except IntegrityError:
+                db.session.rollback()
+                return jsonify(error='Recipe data is not valid'), 422
+        return jsonify(error='Unauthorized'), 401
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
@@ -28,6 +154,6 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
 
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
--- server/models.py
+++ server/models.py
@@ -1,14 +1,58 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-
+from sqlalchemy.orm import validates
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(128), nullable=False)
+    image_url = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+
+    def __init__(self, username, password, image_url=None, bio=None):
+        self.username = username
+        self.set_password(password)
+        self.image_url = image_url
+        self.bio = bio
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hash is not readable")
+
+    def set_password(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    @validates('username')
+    def validate_username(self, key, value):
+        if User.query.filter(User.username == value).first() is not None:
+            raise ValueError("Username already exists.")
+        return value
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
-    pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    instructions = db.Column(db.String(1000), nullable=False)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+
+    @validates('title')
+    def validate_title(self, key, value):
+        if not value:
+            raise ValueError("Title must be present.")
+        return value
+
+    @validates('instructions')
+    def validate_instructions(self, key, value):
+        if not value or len(value) < 50:
+            raise ValueError("Instructions must be present and at least 50 characters long.")
+        return value
